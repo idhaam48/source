9442411d93213f7726112dc12dc55aa346beb222
idhaam <52408837+idhaam@users.noreply.github.com>
2019-07-02 23:08:37 +0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,35 @@
+$(function ()
+    {
+        $('#txt').keyup(function (e){
+            if(e.keyCode == 13){
+                var curr = getCaret(this);
+                var val = $(this).val();
+                var end = val.length;
+
+                $(this).val( val.substr(0, curr) + '<br>' + val.substr(curr, end));
+            }
+
+        })
+    });
+
+    function getCaret(el) { 
+        if (el.selectionStart) { 
+            return el.selectionStart; 
+        }
+        else if (document.selection) { 
+            el.focus(); 
+
+            var r = document.selection.createRange(); 
+            if (r == null) { 
+                return 0; 
+            } 
+
+            var re = el.createTextRange(), 
+            rc = re.duplicate(); 
+            re.moveToBookmark(r.getBookmark()); 
+            rc.setEndPoint('EndToStart', re); 
+
+            return rc.text.length; 
+        }  
+        return 0; 
+    }
